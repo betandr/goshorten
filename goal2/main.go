661c917f7c4c8df4d1bbcc01dacaeb65fea6a683
Beth Anderson <beth.anderson@bbc.co.uk>
2020-05-01 16:26:08 +0100

    workshop

--- goal2/main.go
+++ goal2/main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
 	"net/http"
 )
 
@@ -12,33 +9,15 @@ type EncodeRequest struct {
 	URL string
 }
 
+// EncodeResponse represents the result of encoding
+type EncodeResponse struct {
+	ShortURL string
+}
+
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		w.WriteHeader(http.StatusNotImplemented)
-		fmt.Fprintf(w, "NOT IMPLEMENTED")
-
-	case "POST":
-		jsonBody := json.NewDecoder(r.Body)
-		var encReq EncodeRequest
-		jsonBody.Decode(&encReq)
-
-		if len(encReq.URL) > 0 {
-			w.WriteHeader(http.StatusOK)
-			w.Header().Set("Content-Type", "application/json")
-
-			fmt.Fprintf(w, "GOT: %s", encReq.URL)
-			return
-		}
-
-		w.WriteHeader(http.StatusBadRequest)
-		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprintf(w, " { \"error\": \"No URL found\" }")
-	}
 
 }
